d3f6515f5c9bbe945c2c2d19dded1cb4629d8532
Prasad Bhatane <37994470+prasadbhatane@users.noreply.github.com>
2020-07-14 17:04:29 +0530

    Update graphy_plotter.py

--- graphy_plotter.py
+++ graphy_plotter.py
@@ -1,8 +1,10 @@
+# Created By : Prasad Bhatane #
+
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Dictionary to used to map string into valid string
+# Dictionary used to map string into valid string
 to_be_replaced = {
 	'sin' : "np.sin",
 	'cos' : 'np.cos',
@@ -14,7 +16,7 @@ to_be_replaced = {
 }
 
 st.title("GRAPHY PLOTTER")
-if st.checkbox("Show User Guide", False, key='123'):	
+if st.checkbox("Show User Guide", False, key='123'):	#User Guide
 	st.markdown("- GRAPHY PLOTTER is an app that lets you plot as many equations you want on the same graph")
 	st.markdown("- Look into sidebar to enter the no. of equations you want to plot")
 	st.markdown("- As you enter the number it creates a text box where you can type your equation in x")
@@ -22,9 +24,10 @@ if st.checkbox("Show User Guide", False, key='123'):
 	st.markdown("- You can also adjust the range of x variable by checking Enter The X Range in sidebar")
 	st.markdown("- Once you read all instructions above, you may want to uncheck the SHhow User Guide")
 
-st.sidebar.title("GRAPHY PLOTTER : MENU")
+st.sidebar.title("GRAPHY PLOTTER : MENU")          # Sidebar title
 
-X_from = -100
+# Defaut values of x
+X_from = -100 
 X_to = 100
 
 if st.sidebar.checkbox("Enter The X Range", False, key='1'):
@@ -54,7 +57,7 @@ if show_graph_button:
 				eqn_arr[ind] = eqn_arr[ind].replace(old_keyword, to_be_replaced[old_keyword])
 			plt.plot(x, eval(eqn_arr[ind]), label=lb)
 	except SyntaxError:
-		st.markdown("### 🔔  Please enter a valid equation in Equation Box {} !!!!".format(ind+1))
+		st.markdown("### 🔔  Please enter a valid equation in Equation Box {} !!!!".format(ind+1))   # Notification 
 	
 	plt.xlabel("X")
 	plt.ylabel("Y")
